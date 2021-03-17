637c17077386deacf7c1f1a271c84af7d7ea0c07
Eric Fritz <eric@eric-fritz.com>
2021-03-17 16:06:08 -0500

    Update linker methods.

--- src/linker.ts
+++ src/linker.ts
@@ -136,6 +136,48 @@ export class ExportLinker extends Linker {
     }
   }
 
+  public handleMoniker2(identifier: string): Moniker | undefined {
+    let tscMoniker: TscMoniker = TscMoniker.parse(identifier)
+    if (
+      TscMoniker.hasPath(tscMoniker) &&
+      this.isPackaged(path.join(this.projectRoot, tscMoniker.path))
+    ) {
+      this.ensurePackageInformation()
+      let npmIdentifier: string
+      if (
+        this.packageJson.main === tscMoniker.path ||
+        this.packageJson.typings === tscMoniker.path
+      ) {
+        npmIdentifier = NpmMoniker.create(
+          this.packageJson.name,
+          undefined,
+          tscMoniker.name
+        )
+      } else {
+        npmIdentifier = NpmMoniker.create(
+          this.packageJson.name,
+          tscMoniker.path,
+          tscMoniker.name
+        )
+      }
+      let npmMoniker = this.createMoniker(
+        npmIdentifier,
+        MonikerKind.export,
+        NpmMoniker.scheme
+      )
+      this.emit(npmMoniker)
+      this.emit(
+        this.createPackageInformationEdge(
+          npmMoniker.id,
+          this.packageInformation!.id
+        )
+      )
+      return npmMoniker
+    }
+
+    return undefined
+  }
+
   private isPackaged(uri: string): boolean {
     // This needs to consult the .npmignore file and checks if the
     // document is actually published via npm. For now we return
@@ -240,5 +282,83 @@ export class ImportLinker extends Linker {
       )
       this.emit(this.createNextMonikerEdge(npmMoniker.id, id))
     }
+  }
+
+  public handleMoniker2(identifier: string): Moniker | undefined {
+    const tscMoniker = TscMoniker.parse(identifier)
+    if (!TscMoniker.hasPath(tscMoniker)) {
+      return undefined
+    }
+    let parts = tscMoniker.path.split('/')
+    let packagePath: string | undefined
+    let monikerPath: string | undefined
+    for (let i = parts.length - 1; i >= 0; i--) {
+      let part = parts[i]
+      if (part === 'node_modules') {
+        // End is exclusive and one for the name
+        const packageIndex = i + (parts[i + 1].startsWith('@') ? 3 : 2)
+        packagePath = path.join(
+          this.projectRoot,
+          ...parts.slice(0, packageIndex),
+          `package.json`
+        )
+        monikerPath = parts.slice(packageIndex).join('/')
+        break
+      }
+    }
+    if (
+      packagePath === undefined ||
+      (monikerPath !== undefined && monikerPath.length === 0)
+    ) {
+      return undefined
+    }
+    let packageData = this.packageData.get(packagePath)
+    if (packageData === undefined) {
+      let packageJson = PackageJson.read(packagePath)
+      if (packageJson === undefined) {
+        this.packageData.set(packagePath, null)
+      } else {
+        packageData = {
+          packageInfo: this.createPackageInformation(packageJson),
+          packageJson: packageJson,
+        }
+        this.emit(packageData.packageInfo)
+        this.packageData.set(packagePath, packageData)
+      }
+    }
+    if (packageData !== null && packageData !== undefined) {
+      let npmIdentifier: string
+      if (
+        packageData.packageJson.typings === monikerPath ||
+        packageData.packageJson.main === monikerPath
+      ) {
+        npmIdentifier = NpmMoniker.create(
+          packageData.packageJson.name,
+          undefined,
+          tscMoniker.name
+        )
+      } else {
+        npmIdentifier = NpmMoniker.create(
+          packageData.packageJson.name,
+          monikerPath,
+          tscMoniker.name
+        )
+      }
+      let npmMoniker = this.createMoniker(
+        npmIdentifier,
+        MonikerKind.import,
+        NpmMoniker.scheme
+      )
+      this.emit(npmMoniker)
+      this.emit(
+        this.createPackageInformationEdge(
+          npmMoniker.id,
+          packageData.packageInfo.id
+        )
+      )
+      return npmMoniker
+    }
+
+    return undefined
   }
 }
